da7f42d30f4acf0c9e41dbc6efaf98654b8cb45b
katie stout <katiestout@katies-MBP.home>
2018-06-28 21:53:58 -0400

    somechanges

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgerstable (burger_name, devoured) VALUES ("Whale this isn't acutally a burger!", "0");
+
+INSERT INTO burgerstable (burger_name, devoured) VALUES ("Holy Cow! Or Holy Cat?", "0");
+
+INSERT INTO burgerstable (burger_name, devoured) VALUES ("Boring Vegetarian Burger.", "0");
